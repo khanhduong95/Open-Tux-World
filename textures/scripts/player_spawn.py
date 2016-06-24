73020277c129f647885b0be21b2715fd187a77b7
khanhduong95 <khanhduongdv@gmail.com>
2016-06-25 00:18:05 +0700

    remove executables

--- textures/scripts/player_spawn.py
+++
@@ -1,7 +0,0 @@
-def main(cont):
-    own = cont.owner
-
-    if own["init"]:
-        cont.activate(cont.actuators["spawn"])
-        own["init"] = False
-        cont.deactivate(cont.actuators["spawn"])
