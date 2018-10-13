78e6a9d62a69d2459014c247fafce14a0b3158aa
duong <khanhduongdv@gmail.com>
2018-10-13 11:15:09 +0700

    Revert "edit"
    This reverts commit bbe186d2b77732820b1421a9fad9059f8e247a58.

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import platform
 ops = bpy.ops
 wm = ops.wm
 
-bpy.data.scenes["Scene"].game_settings.show_fullscreen = True
+# bpy.data.scenes["Scene"].game_settings.show_fullscreen = True
 
 os_name = platform.system()
 if os_name == "Windows":
